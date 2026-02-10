50b47f0125622d5ee5d7428a95b4de6d055f86ff
kradmoned <110401321+kradmoned@users.noreply.github.com>
2026-02-10 19:51:38 +0500

    Draw player character

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT,PLAYER_RADIUS,LINE_WIDTH
 from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     pygame.init()
     display = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
+        dt = clock.tick(60)/1000
+        #print(dt)
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             display.fill("black")
+            player.draw(display)
             pygame.display.flip()
 
 
